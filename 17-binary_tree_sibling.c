398b598bf160643858654dbce2d2fca5c6e73d87
Thapelo-ST <tsamkelo96@gmail.com>
2023-08-30 06:09:44 +0200

    hot fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
-		return NULL;
+		return (NULL);
 
 	if (node->parent->left == node)
 		return (node->parent->right);
